7c118d913391b6baeb3f7f247ae7f82ff50b50ce
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-06-25 16:21:31 +0200

    Stampati in pagina i post con Vue

--- resources/js/posts.js
+++ resources/js/posts.js
@@ -1,1 +1,15 @@
-alert('test');
+const { default: Axios } = require("axios")
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        posts: []
+    },
+    mounted() {
+        axios.get('http://127.0.0.1:8000/api/posts')
+            .then(result => {
+                this.posts = result.data.posts
+                console.log(this.posts);
+            })
+    }
+}) 
--- resources/views/guest/posts/vue-posts.blade.php
+++ resources/views/guest/posts/vue-posts.blade.php
@@ -11,24 +11,26 @@
 
 @section('content')
     <div class="container">
+        {{-- Vue Root --}}
+        <div id="root">
 
-        <h1 class="mb-4">Tutti i post:</h1>
+            <h1 class="mb-4">Tutti i post</h1>
 
-        <div class="row">
-            
-            {{-- @foreach ($posts as $post)
-                <div class="col-4 mb-3">
+            <div class="row">    
+
+                <div class="col-4 mb-3" v-for="post in posts">
                     <div class="card" style="width: 18rem;">
                         <div class="card-body">
-                          <h5 class="card-title">{{ ucfirst($post->title) }}</h5>
-                          <p class="card-text">{{ substr($post->content, 0, 50) }}...</p>
-                          <a href="{{ route('show', ['slug' => $post->slug]) }}" class="btn btn-primary">Leggi il post</a>
+                            <h5 class="card-title">@{{ post.title }}</h5>
+                            <p class="card-text">@{{ post.content }}...</p>
+                            <a href="#" class="btn btn-primary">Leggi il post</a>
                         </div>
-                      </div>
+                    </div>
                 </div>
-            @endforeach --}}
 
-        </div>
+            </div>
+            
+        </div>        
     </div>
 @endsection
 
